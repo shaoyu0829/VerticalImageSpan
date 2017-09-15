749d5b93196f713ff8de767a8dfa7e29bd8d0f51
shaoyu0829 <shaoyu@lppz.com>
2017-09-15 16:08:44 +0800

    My First Commit

---
+++ app/src/main/java/com/span/ssy/verticalimagespan/MainActivity.java
@@ -0,0 +1,17 @@
+package com.span.ssy.verticalimagespan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = ((TextView) findViewById(R.id.tv));
+    }
+}
